da45547ce7c8be4ea5f42e3b611e83c4c19fe3ca
Wendel Sagrilo <wendelsagrilo@gmail.com>
2018-10-17 19:09:04 -0300

    Revert "Updates"

---
+++ react/src/Components/GoogleLogin.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+
+class LoginGoogle extends React.Component {
+
+
+    render() {
+        return (
+            <GoogleLogin
+                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                buttonText="Entrar com Gmail"
+                className="googleBtn"
+            /> 
+        )
+    }
+
+}
+export default LoginGoogle;
